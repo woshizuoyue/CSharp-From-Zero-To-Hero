97c25fabf24293edb7bd3029c6b3788a8077d710
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-09-22 11:54:26 -0400

    homework1

---
+++ Src/BootCamp.Chapter/Lesson1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+
+namespace BootCamp.Chapter
+{
+    class Lesson1
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Enter the age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Enter the weights: ");
+            string weight = Console.ReadLine();
+            Console.WriteLine("Enter the heights: ");
+            string height = Console.ReadLine();
+
+            Console.WriteLine($"Tom Jefferson is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+        }
+    }
+}
--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -1,19 +1,12 @@
-﻿using System;
-
+﻿
 namespace BootCamp.Chapter
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the age: ");
-            string age = Console.ReadLine();
-            Console.WriteLine("Enter the weights: ");
-            string weight = Console.ReadLine();
-            Console.WriteLine("Enter the heights: ");
-            string height = Console.ReadLine();
-
-            Console.WriteLine($"Tom Jefferson is {age} years old, his weight is {weight} kg and his height is {height} cm.");
+            Lesson1 lesson1 = new Lesson1();
+            lesson1.Demo();
         }
     }
 }
