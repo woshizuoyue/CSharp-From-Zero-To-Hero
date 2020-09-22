bd952dc99aa645e669db138714818e9843610680
Yue Zuo <woshizuoyue9201@yahoo.com>
2020-09-22 11:42:27 -0400

    first commit

--- Src/BootCamp.Chapter/Program.cs
+++ Src/BootCamp.Chapter/Program.cs
@@ -6,7 +6,14 @@ namespace BootCamp.Chapter
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter the age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Enter the weights: ");
+            string weight = Console.ReadLine();
+            Console.WriteLine("Enter the heights: ");
+            string height = Console.ReadLine();
+
+            Console.WriteLine($"Tom Jefferson is {age} years old, his weight is {weight} kg and his height is {height} cm.");
         }
     }
 }
